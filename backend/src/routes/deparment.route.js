68ecd5590a82be0bf235ecc1cf5dad9d2b4f25ec
Nelson2023-2024 <nelsonobuya19@gmail.com>
2025-05-01 14:31:51 +0300

    Get all departments functionality completed

--- backend/src/routes/deparment.route.js
+++ backend/src/routes/deparment.route.js
@@ -3,6 +3,19 @@ import { Department } from "../models/Department.model";
 
 const router = Router();
 
+router.get("/", async (req,res) => {
+  try {
+    const departments = await Department.find({});
+    res.status(200).json({ departments });
+  } catch (error) {
+    console.log(
+      "An error occurred in the get-all-departments route:",
+      error.message
+    );
+    res.status(500).json({ message: "Internal server error", error: error.message });
+  }
+})
+
 router.post("/create-department", async (req, res) => {
   try {
     const { name, description } = req.body;
