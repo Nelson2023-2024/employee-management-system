161ebeab29dc2bc2926b504929ef62b6e457f518
Nelson2023-2024 <nelsonobuya19@gmail.com>
2025-05-01 14:28:01 +0300

    Established connection with MongoDB

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import  { configDotenv } from "dotenv"
 import { authRoutes } from "./routes/auth.route.js"
+import { connectToDB } from "./lib/db.js"
 configDotenv()
 
 const PORT = process.env.PORT || 5005
@@ -11,4 +12,5 @@ app.use(express.json())
 app.use("/api/auth", authRoutes)
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
+    connectToDB()
 })
