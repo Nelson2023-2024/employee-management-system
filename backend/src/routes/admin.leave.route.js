92f3bc71dd3e9eb81716241d997774262db0094d
Nelson2023-2024 <nelsonobuya19@gmail.com>
2025-05-02 09:49:07 +0300

    Create Leave funcionality completed

--- backend/src/routes/admin.leave.route.js
+++ backend/src/routes/admin.leave.route.js
@@ -1,3 +1,40 @@
 import { Router } from "express";
+import { adminRoute, protectRoute } from "../middleware/protectRoute.js";
+import { LeaveType } from "../models/LeaveType.model.js";
 
-const 
+const router = Router();
+
+router.use(protectRoute, adminRoute);
+
+//create a leave
+router.post("/", async (req, res) => {
+  try {
+    const { name, description, maxDays } = req.body;
+
+    if (!name || !description || !maxDays)
+      return res.status(400).json({ message: "All fields are required" });
+
+    const leaveExist = await LeaveType.findOne({ name });
+    if (leaveExist)
+      return res.status(400).json({ message: "Leave already exists" });
+
+    //if the leave doesn't exist
+    const leave = await LeaveType.create({
+      name,
+      description,
+      maxDays,
+    });
+
+    res.status(201).json({ message: "Leave Created successfully", leave });
+  } catch (error) {
+    console.log(
+      "An error occurred in the create-leave-route route:",
+      error.message
+    );
+    res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
+  }
+});
+
+export { router as adminLeaveRoutes };
